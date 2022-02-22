69c07630ab98ae657481c03f5b648f360d6965e7
= <=>
2022-02-23 00:39:34 +0100

    feat: add pane between option

--- src/components/common/pane/Pane.tsx
+++ src/components/common/pane/Pane.tsx
@@ -1,15 +1,20 @@
+import classNames from 'classnames';
 import React, { FunctionComponent, ReactNode } from 'react';
 
 const className = {
   root: `px-4 py-3 rounded bg-gray-100`,
+  between: `flex justify-between items-center`,
 };
 
 interface PaneProps {
+  between: boolean;
   children: ReactNode;
 }
 
-const Pane: FunctionComponent<PaneProps> = ({ children }) => (
-  <div className={className.root}>{children}</div>
+const Pane: FunctionComponent<PaneProps> = ({ between, children }) => (
+  <div className={classNames(className.root, between && className.between)}>
+    {children}
+  </div>
 );
 
 export default Pane;
