38061cbee268440f034d21d4a7ca8acfbdf89e62
= <=>
2022-02-22 17:18:37 +0100

    feat: text sizes

--- src/components/text/Text.tsx
+++ src/components/text/Text.tsx
@@ -1,18 +1,40 @@
 import classNames from 'classnames';
 import React, { FunctionComponent, ReactNode } from 'react';
 
-const className = {
-  root: `text-sm text-gray-900`,
-  muted: `text-gray-500`,
-};
+export enum TextSizes {
+  Small = 'small',
+  Medium = 'medium',
+  Large = 'large',
+}
 
 interface TextProps {
+  size?: TextSizes;
   muted?: boolean;
   children: ReactNode;
 }
 
-const Text: FunctionComponent<TextProps> = ({ muted = false, children }) => (
-  <div className={classNames(className.root, muted && className.muted)}>
+const className = {
+  root: `text-gray-900`,
+  muted: `text-gray-500`,
+  sizes: {
+    [TextSizes.Small]: 'text-sm',
+    [TextSizes.Medium]: 'text-md',
+    [TextSizes.Large]: 'text-lg',
+  },
+};
+
+const Text: FunctionComponent<TextProps> = ({
+  size = TextSizes.Medium,
+  muted = false,
+  children,
+}) => (
+  <div
+    className={classNames(
+      className.root,
+      className.sizes[size],
+      muted && className.muted
+    )}
+  >
     {children}
   </div>
 );
