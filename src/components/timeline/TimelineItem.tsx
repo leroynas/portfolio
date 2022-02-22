9f74f9885ef21fae6e13039e56c85467c1de64f3
= <=>
2022-02-23 00:24:26 +0100

    feat: add spacing below timeline item header

--- src/components/timeline/TimelineItem.tsx
+++ src/components/timeline/TimelineItem.tsx
@@ -4,7 +4,7 @@ const className = {
   root: `relative pb-10 ml-6 -translate-y-4 last:pb-0`,
   avatar: `absolute -left-8 top-5 h-4 w-4 rounded-full bg-blue-500 ring-8 ring-white`,
   container: `rounded-lg border border-gray-200 bg-white p-4 shadow-sm`,
-  heading: `items-center justify-between sm:flex`,
+  heading: `items-center justify-between sm:flex mb-4 last:mb-0`,
   meta: `block mb-1 text-sm font-normal text-gray-400 sm:order-last sm:mb-0`,
   title: `flex text-md font-semibold`,
   content: `mt-2`,
