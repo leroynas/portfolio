0f8462c7eee0e47643e4134358aba7279dce5c2e
= <=>
2022-02-24 00:42:24 +0100

    feat: configure gatsby icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,18 +16,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#3B82F6`,
+        theme_color: `#3B82F6`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`,
+      },
+    },
     `gatsby-plugin-postcss`,
   ],
 };
